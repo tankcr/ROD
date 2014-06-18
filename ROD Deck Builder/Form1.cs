d32372d894db5294949504cb545ea008da080be4
tankcr <kristopher.roy@gmail.com>
2014-06-18 16:01:34 -0700

    fixed form1.cs

--- ROD Deck Builder/Form1.cs
+++ ROD Deck Builder/Form1.cs
@@ -24,7 +24,6 @@ namespace ROD_Deck_Builder
 //            //
 //            foreach(HtmlNode table in webdoc.DocumentNode.SelectNodes("//a[@class=]"))
 //            {HtmlNode newpagedata = table;}
-<<<<<<< HEAD
             //Card items = (newpage.TableData).Items[0].Name;
             //List<ROD_Deck_Builder.EFaction> efactions = (newpage.TableData.Select(x => x.Faction).ToList());
             //List<string> factions = new List<string>();
@@ -34,11 +33,8 @@ namespace ROD_Deck_Builder
             listBox1.DataSource = (newpage.TableData.Select(x => x.Faction).ToList());
             
             //(new System.Collections.Generic.Mscorlib_CollectionDebugView<ROD_Deck_Builder.Card>(newpage.TableData)).Items[0].Name;    
-=======
                 listBox1.DisplayMember = "Faction";
                 listBox1.DataSource = (newpage.TableData.Select(x => x.Faction).ToList());
-
->>>>>>> 12f9156719031be04c4e9f5d0a6f9ac706759dd0
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
